ebda58bf1076626119c7897542ead28ba5f47813
Olrsted <roy.jiang@student.tdsb.on.ca>
2024-12-16 01:47:22 -0500

    added register and cart

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,34 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import './App.css';
 import HomeView from "./views/HomeView.jsx";
-import LoginView from "./views/LoginView.jsx";
 import RegisterView from "./views/RegisterView.jsx";
+import LoginView from "./views/LoginView.jsx";
 import MoviesView from "./views/MoviesView.jsx";
 import GenreView from "./views/GenreView.jsx";
 import DetailView from "./views/DetailView.jsx";
-
+import CartView from "./views/CartView.jsx";
+import SettingsView from "./views/SettingsView.jsx";
+import { StoreProvider } from "./context";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<HomeView />} />
-        <Route path="/signIn" element={<LoginView />} />
-        <Route path="/signUp" element={<RegisterView />} />
-        <Route path="/movies" element={<MoviesView />}>
-          <Route path="genre/:genre_id" element={<GenreView />} />
-          <Route path="details/:id" element={<DetailView />} />
-        </Route>
-      </Routes>
-    </Router>
-  );
+    <StoreProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomeView />} />
+          <Route path="/register" element={<RegisterView />} />
+          <Route path="/login" element={<LoginView />} />
+          <Route path="/movies" element={<MoviesView />}>
+            <Route path="genre/:id" element={<GenreView />} />
+            <Route path="details/:id" element={<DetailView />} />
+          </Route>
+          <Route path="/cart" element={<CartView/>}/>
+          <Route path="/settings" element={<SettingsView/>}/>
+        </Routes>
+      </BrowserRouter>
+    </StoreProvider>
+
+  )
 }
 
 export default App;
